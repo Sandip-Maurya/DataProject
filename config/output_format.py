ade7789882e47565bd55c8219870e04073bc96ba
Sandip Maurya <skmparsipur@gmail.com>
2024-12-12 15:07:08 +0530

    Update .gitignore to exclude unnecessary files and directories

--- config/output_format.py
+++
@@ -1,7 +0,0 @@
-# utils/output_format.py
-
-from config.config import OUTPUT_FORMAT_PATH
-import pandas as pd
-
-output_df = pd.read_csv(OUTPUT_FORMAT_PATH)
-output_df.set_index('student_subject_id', inplace=True)
